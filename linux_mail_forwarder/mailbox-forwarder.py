0ae9c4960648d931c5f298c06cae41a45f980298
Adam Hlaváček <git@adamhlavacek.com>
2024-02-03 09:37:24 +0100

    feat: attach original message

--- linux_mail_forwarder/mailbox-forwarder.py
+++ linux_mail_forwarder/mailbox-forwarder.py
@@ -16,7 +16,7 @@ gpg = gnupg.GPG()
 
 # Load variables from .env file
 smtp_server = os.getenv('SMTP_SERVER')
-smtp_port = int(os.getenv('SMTP_PORT'))  # Ensure this is an integer
+smtp_port = int(os.getenv('SMTP_PORT') or "465")  # Ensure this is an integer
 smtp_user = os.getenv('SMTP_USER')
 smtp_password = os.getenv('SMTP_PASSWORD')
 from_address = smtp_user
@@ -39,10 +39,12 @@ def forward_email(msg: mailbox.mboxMessage) -> bool:
     """
     # Encrypt the email content with the recipient's public key
     # noinspection PyTypeChecker
-    encrypted_data = gpg.encrypt(msg.get_payload(), recipients=[to_address], always_trust=True)
+    encrypted_content = gpg.encrypt(msg.get_payload(), recipients=[to_address], always_trust=True, armor=True)
+    # noinspection PyTypeChecker
+    encrypted_mail = gpg.encrypt(msg.as_bytes(), recipients=[to_address], always_trust=True, armor=True)
 
-    if not encrypted_data.ok:
-        print(f"Encryption failed: {encrypted_data.status}")
+    if not encrypted_content.ok:
+        print(f"[!] encryption failed: {encrypted_content.status}")
         return False
 
     with smtplib.SMTP_SSL(smtp_server, smtp_port, timeout=30) as server:
@@ -50,7 +52,14 @@ def forward_email(msg: mailbox.mboxMessage) -> bool:
 
         # Create a new EmailMessage for forwarding, with encrypted content
         forward_msg = EmailMessage()
-        forward_msg.set_content(str(encrypted_data))
+        forward_msg.set_content(str(encrypted_content))
+
+        forward_msg.add_attachment(
+            encrypted_mail.data,
+            filename="original_message.eml.asc",
+            maintype="application",
+            subtype="pgp-encrypted"
+        )
 
         # Set the headers for forwarding
         forward_msg['Subject'] = f"[DEV] [{gethostname()}] {msg.get('subject', 'No Subject')}"
@@ -71,7 +80,6 @@ def main() -> int:
         # Iterate over all messages in the mailbox and forward them
         for msg in mbox:
             forward_email(msg)
-            print("Email forwarded successfully.")
     except FileNotFoundError:
         print(f"The file {mailbox_path} does not exist.")
         return 1
